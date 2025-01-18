edd2c4382b9b9b4d150f8fc48e0b617ce413c651
AnushkaNegi27 <anushka27.negi@gmail.com>
2025-01-18 10:41:17 +0530

    Add chatbot project files

---
+++ cse_chatbot.py
@@ -0,0 +1,64 @@
+import random
+import json
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from nltk_utils import bag_of_words, tokenize
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+data = torch.load("cse_chatbot.pth", weights_only=True)
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNet, self).__init__()
+        self.l1 = torch.nn.Linear(input_size, hidden_size)
+        self.l2 = torch.nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = torch.relu(self.l1(x))
+        x = self.l2(x)
+        return x
+
+model = NeuralNet(input_size, hidden_size, output_size)
+model.load_state_dict(data["model_state"])
+model.eval()
+
+all_words = data["all_words"]
+tags = data["tags"]
+with open('cse_chatbot_dataset.json', 'r') as f:
+    intents = json.load(f)
+
+chatbot_name = "GEHU"
+print("Hello! Type 'quit' to exit.")
+while True:
+    try:
+        sentence = input('You: ')
+        if sentence.lower() == "quit":
+            print("Goodbye!")
+            break
+
+        tokenized = tokenize(sentence)
+        bag = bag_of_words(tokenized, all_words)
+        bag = bag.reshape(1, bag.shape[0])
+        bag = torch.from_numpy(bag)
+
+        output = model(bag)
+        _, predicted = torch.max(output, dim=1)
+        tag = tags[predicted.item()]
+        probs = torch.softmax(output, dim=1)
+        prob = probs[0][predicted.item()]
+        if prob.item() > 0.75:
+            for intent in intents["intents"]:
+                if tag == intent["tag"]:
+                    response = random.choice(intent['responses'])
+                    print(f"{chatbot_name}: {response}")
+        else:
+            print(f"{chatbot_name}: Sorry, I don't know about that.")
+    except KeyboardInterrupt:
+        print("\nGoodbye!")
+        break
